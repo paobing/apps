19a4b8fb1faa1025d3417374ed40e59c941a964c
paobing <paobing_guo@163.com>
2023-03-03 19:15:16 +0800

    数据层接口文件命名说明； 单元测试文件编写以及映射；

---
+++ todos/src/test/java/com/paobing/dao/UserDaoTest.java
@@ -0,0 +1,13 @@
+package com.paobing.dao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserDaoTest {
+
+    @Test
+    void testGetById(){
+        System.out.println("testing...");
+    }
+}
