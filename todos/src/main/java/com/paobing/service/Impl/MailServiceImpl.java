ae7d670921464e7972475fda59206af752fd2120
paobing <paobing_guo@163.com>
2023-03-16 11:27:03 +0800

    发送带附件邮件功能

--- todos/src/main/java/com/paobing/service/Impl/MailServiceImpl.java
+++ todos/src/main/java/com/paobing/service/Impl/MailServiceImpl.java
@@ -1,9 +1,12 @@
 package com.paobing.service.Impl;
 
 import com.paobing.service.MailService;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,11 +22,29 @@ public class MailServiceImpl implements MailService {
 
     @Override
     public void sendMail() {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("paobing_guo@163.com");
-        message.setTo("xiangni_zhao@126.com");
-        message.setSubject("Hello～");
-        message.setText("你好啊～");
+        // 简易邮件
+        //SimpleMailMessage message;
+        //message = new SimpleMailMessage();
+        //message.setFrom("paobing_guo@163.com");
+        //message.setTo("xiangni_zhao@126.com");
+        //message.setSubject("Hello～");
+        //message.setText("你好啊～");
+
+        // 复杂邮件 Text支持html
+        MimeMessage message = null;
+        try {
+            message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+            helper.setFrom("paobing_guo@163.com");
+            helper.setTo("xiangni_zhao@126.com");
+            helper.setSubject("Hello～");
+            helper.setText("你好啊～", true);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
+
         javaMailSender.send(message);
         System.out.println("Send Mail...");
     }
